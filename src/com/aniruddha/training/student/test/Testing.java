6cdc6dd6c1cbce09a11ce10cf266589cb195f599
Aniruddha <anigayake101@gmail.com>
2022-04-04 11:23:01 +0530

    second

--- src/com/aniruddha/training/student/test/Testing.java
+++ src/com/aniruddha/training/student/test/Testing.java
@@ -8,17 +8,25 @@ import com.aniruddha.training.student.Student;
 
 class Testing {
 	Student s= new Student(100, 95, 95);
+	Student s1= new Student(85,85,85);
+	 Student s2= new Student(75,75,75);
+	 
 	@Test
 	void testStudentGradeA() {
 		assertEquals(s.calculateGrade(), "A");
 	}
-	Student s1= new Student(85,85,85);
+	
+	
+	@Test
+	
 	 void testGradeB() {
 		 assertEquals(s1.calculateGrade(), "B");
 	 }
-
-	 Student s2= new Student(75,75,75);
+	 
+	 
+	@Test
 	 void testGradeC() {
 		 assertEquals(s1.calculateGrade(), "C");
 	 }
+ 
 }
